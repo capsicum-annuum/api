affa560980aaf24f597b3db9e4354686deb3eee4
Luis Henrique Hendges <luishendges@outlook.com>
2020-04-04 01:51:13 -0300

    Initial Commit

---
+++ src/main/java/br/com/annuum/capsicum/api/controller/ApplicationController.java
@@ -0,0 +1,14 @@
+package br.com.annuum.capsicum.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class ApplicationController {
+
+    @GetMapping
+    public ResponseEntity verify() {
+        return ResponseEntity.ok().body("ok!");
+    }
+}
---
+++ src/test/java/br/com/annuum/capsicum/api/ApiApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.annuum.capsicum.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+
+}
