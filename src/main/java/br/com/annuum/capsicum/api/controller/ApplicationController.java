6dde846f4108ae8a42744bde92f7967f2cf931b5
thales-gelinger <thalesgelinger99@gmail.com>
2020-04-04 11:02:07 -0300

    build: trigger deploy

--- src/main/java/br/com/annuum/capsicum/api/controller/ApplicationController.java
+++ src/main/java/br/com/annuum/capsicum/api/controller/ApplicationController.java
@@ -9,6 +9,6 @@ public class ApplicationController {
 
     @GetMapping
     public ResponseEntity verify() {
-        return ResponseEntity.ok().body("ok!");
+        return ResponseEntity.ok().body("OK!");
     }
 }
