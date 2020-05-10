7c8dde2879d22b15d1edc5a900afddafbb0e5ae8
Gabriel Tamujo Meyrer <gabrieltamujo@outlook.com>
2020-05-09 21:31:05 -0300

    feat: custom constraint annotation

--- src/main/java/br/com/annuum/capsicum/api/domain/Movement.java
+++ src/main/java/br/com/annuum/capsicum/api/domain/Movement.java
@@ -1,5 +1,6 @@
 package br.com.annuum.capsicum.api.domain;
 
+import br.com.annuum.capsicum.api.annotation.DateStartBeforeDateEnd;
 import br.com.annuum.capsicum.api.listener.AttributeEncodeListener;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -15,6 +16,7 @@ import java.util.List;
 @EntityListeners(AttributeEncodeListener.class)
 @Entity
 @SequenceGenerator(name = "post_sequence", sequenceName = "post_sequence", allocationSize = 1)
+@DateStartBeforeDateEnd
 public class Movement {
 
     @Id
