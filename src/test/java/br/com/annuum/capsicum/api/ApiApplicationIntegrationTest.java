30842a7ed756a21c7a57ffbeb6c5261d6239107d
Gabriel Tamujo Meyrer <gabrieltamujo@outlook.com>
2020-05-09 21:54:51 -0300

    fixes

--- src/test/java/br/com/annuum/capsicum/api/ApiApplicationIntegrationTest.java
+++ src/test/java/br/com/annuum/capsicum/api/ApiApplicationIntegrationTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("test")
-public class ApiCandidacyIntegrationTest {
+public class ApiApplicationIntegrationTest {
 
     @Resource
     private CauseRepository causeRepository;
--- src/test/java/br/com/annuum/capsicum/api/controller/ApplicationControllerIntegrationTest.java
+++ src/test/java/br/com/annuum/capsicum/api/controller/ApplicationControllerIntegrationTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class CandidacyControllerIntegrationTest {
+public class ApplicationControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
