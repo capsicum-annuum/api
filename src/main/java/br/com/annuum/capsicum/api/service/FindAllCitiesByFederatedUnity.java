8e77eac0fa78eb20e116953a9e23618dbee1d735
Gabriel Tamujo Meyrer <gabrieltamujo@outlook.com>
2020-06-13 15:28:39 -0300

    feat: list cities by fu service

---
+++ src/main/java/br/com/annuum/capsicum/api/service/FindAllCitiesByFederatedUnity.java
@@ -0,0 +1,22 @@
+package br.com.annuum.capsicum.api.service;
+
+import br.com.annuum.capsicum.api.controller.response.CityListResponse;
+import br.com.annuum.capsicum.api.repository.CityRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class FindAllCitiesByFederatedUnity {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    public CityListResponse find(Long idFederatedUnity) {
+        log.info("Searching for all Cities with Federated Unity id '{}'", idFederatedUnity);
+        return new CityListResponse()
+            .setCities(cityRepository.retrieveAllCitiesAsCityDtoByFederatedUnity(idFederatedUnity));
+    }
+
+}
