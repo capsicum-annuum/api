f9a6b49a952a6ddd8684ccdea0c726b90192fe75
Gabriel Meyrer <gabriel.meyrer@cwi.com.br>
2020-06-13 18:29:28 -0300

    Add where to query

--- src/main/java/br/com/annuum/capsicum/api/repository/CityRepository.java
+++ src/main/java/br/com/annuum/capsicum/api/repository/CityRepository.java
@@ -2,7 +2,6 @@ package br.com.annuum.capsicum.api.repository;
 
 import br.com.annuum.capsicum.api.domain.City;
 import br.com.annuum.capsicum.api.domain.dto.CityDto;
-import br.com.annuum.capsicum.api.domain.dto.SkillDto;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,7 +9,7 @@ import java.util.List;
 
 public interface CityRepository extends PagingAndSortingRepository<City, Long> {
 
-    @Query("SELECT new br.com.annuum.capsicum.api.domain.dto.CityDto(c.id, c.name) FROM City c")
+    @Query("SELECT new br.com.annuum.capsicum.api.domain.dto.CityDto(c.id, c.name) FROM City c " + "WHERE c.federatedUnity.id = ?1")
     List<CityDto> retrieveAllCitiesAsCityDtoByFederatedUnity(Long idFederatedUnity);
 
 }
