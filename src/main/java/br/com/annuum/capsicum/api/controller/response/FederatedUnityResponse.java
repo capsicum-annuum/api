0cc964c75d92e00d88282c5c6f096a0a60d71b56
Gabriel Meyrer <gabriel.meyrer@cwi.com.br>
2020-06-13 17:58:55 -0300

    Create federated unit response class

---
+++ src/main/java/br/com/annuum/capsicum/api/controller/response/FederatedUnityResponse.java
@@ -0,0 +1,14 @@
+package br.com.annuum.capsicum.api.controller.response;
+
+import lombok.Data;
+
+@Data
+public class FederatedUnityResponse {
+
+    private Long id;
+
+    private String acronym;
+
+    private String name;
+
+}
