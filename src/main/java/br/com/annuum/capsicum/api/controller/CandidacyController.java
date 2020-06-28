b0ff3fec2f717ddb179d158166a32c79554a6031
Robson Rosa <robsonrosa@outlook.com>
2020-06-28 10:31:49 -0300

    Substituindo string literals por constantes para as roles

--- src/main/java/br/com/annuum/capsicum/api/controller/CandidacyController.java
+++ src/main/java/br/com/annuum/capsicum/api/controller/CandidacyController.java
@@ -1,11 +1,14 @@
 package br.com.annuum.capsicum.api.controller;
 
+import br.com.annuum.capsicum.api.domain.enums.Profile;
 import br.com.annuum.capsicum.api.security.UserPrincipal;
 import br.com.annuum.capsicum.api.service.SaveCandidacyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 @RequestMapping("/candidacy")
@@ -16,6 +19,7 @@ public class CandidacyController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @RolesAllowed(Profile.Names.VOLUNTEER)
     public void saveCandidacy(@AuthenticationPrincipal final UserPrincipal currentUser, @RequestParam final Long idNeed) {
         saveCandidacyService.save(currentUser.getId(), idNeed);
     }
